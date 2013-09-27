96994c72951f795b09276cd6f7017f666bb24f86
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2013-09-27 15:22:08 +0300

    autoupdate background from reddit/r/earthporn

---
+++ background.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import platform
+import urllib
+import urllib2
+import json
+import os
+
+from random import choice
+from subprocess import call
+
+
+url = 'http://www.reddit.com/r/earthporn.json'
+headers = {
+  'User-Agent' : 'PrettyBackgroundBot/0.1'
+}
+directory = 'backgrounds'
+operating_system = platform.system().lower()
+desktop_session = os.environ.get('DESKTOP_SESSION').lower()
+
+
+def earthporn_frontpage():
+    req = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(req)
+    return json.loads(response.read())['data']['children']
+
+
+def get_random_link():
+    link = choice(earthporn_frontpage())['data']
+    url = str(link['url'])
+    if url.startswith('http://imgur') and not extension_exists(url):
+        link['url'] += '.png'
+    return link
+
+
+def extension_exists(url):
+    return url.endswith('.jpg') or url.endswith('.jpeg') or url.endswith('.png')
+
+
+def set_wallpapper():
+    if operating_system == 'linux':
+        if desktop_session == 'gnome':
+            link = get_random_link()
+            file_path = directory + '/' + link['name']
+            urllib.urlretrieve (link['url'], file_path)
+            call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///home/cristian/vimrc/" + file_path])
+        else:
+            print('Unkown desktop manager')
+    elif operating_system == 'windows':
+        pass
+    else:
+        print('OS not supported')
+
+if not os.path.exists(directory):
+      os.makedirs(directory)
+set_wallpapper()
