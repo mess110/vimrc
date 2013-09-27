fc440df790950c1720807d451de3c02050cced4c
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2013-09-27 15:29:27 +0300

    fix hardcoded working directory

--- background.py
+++ background.py
@@ -14,7 +14,8 @@ url = 'http://www.reddit.com/r/earthporn.json'
 headers = {
   'User-Agent' : 'PrettyBackgroundBot/0.1'
 }
-directory = 'backgrounds'
+working_dir = os.path.dirname(os.path.abspath(__file__))
+directory = os.path.join(working_dir, 'tmp')
 operating_system = platform.system().lower()
 desktop_session = os.environ.get('DESKTOP_SESSION').lower()
 
@@ -41,15 +42,17 @@ def set_wallpapper():
     if operating_system == 'linux':
         if desktop_session == 'gnome':
             link = get_random_link()
-            file_path = directory + '/' + link['name']
+            file_path = os.path.join(directory, link['name'])
+            print file_path
             urllib.urlretrieve (link['url'], file_path)
-            call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///home/cristian/vimrc/" + file_path])
+            call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://" + file_path])
         else:
             print('Unkown desktop manager')
     elif operating_system == 'windows':
         pass
     else:
         print('OS not supported')
+
 
 if not os.path.exists(directory):
       os.makedirs(directory)
