16404015dd10bb1e334abbb27268fca754f246e8
Cristian Mircea Messel <mess110@gmail.com>
2014-01-07 20:49:39 +0200

    add git related config

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 rm -rf ~/.vimrc
 cp .vimrc ~
 cp -R .vim ~
+
 cp .conkyrc ~
 chmod +x weather.py
+
+git config --global user.name "Cristian Mircea Messel"
+# git config --global user.email mess110@gmail.com
+git config --global core.editor "vim"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
