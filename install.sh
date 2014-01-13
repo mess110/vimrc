867464a36091f996444898c5c02aaa0cfbb4550a
Cristian Mircea Messel <mess110@gmail.com>
2014-01-13 19:53:35 +0200

    addd dotfiles-ruby
    copies the .ruby-version and .ruby-gemset from vimrc to the current
    directory and edits them

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ git config --global user.name "Cristian Mircea Messel"
 git config --global core.editor "vim"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-chmod +x bin/infinity
-sudo ln -s ~/vimrc/bin/infinity /usr/local/bin/
+chmod +x bin/*
 
-chmod +x bin/auth-rails
+sudo ln -s ~/vimrc/bin/infinity /usr/local/bin/
 sudo ln -s ~/vimrc/bin/auth-rails /usr/local/bin/
+sudo ln -s ~/vimrc/bin/dotfiles-ruby /usr/local/bin/
