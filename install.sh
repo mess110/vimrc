4b52f364675ff5a43999087272968ed7104412d0
Cristian Mircea Messel <mess110@gmail.com>
2014-01-15 21:10:29 +0200

    add vimrc/bin to path instead of symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+mkdir ~/vimrc/tmp
+
 rm -rf ~/.vimrc
 cp .vimrc ~
 cp -R .vim ~
@@ -9,9 +11,9 @@ git config --global user.name "Cristian Mircea Messel"
 # git config --global user.email mess110@gmail.com
 git config --global core.editor "vim"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global core.excludesfile ~/vimrc/.gitignore_global
 
 chmod +x bin/*
 
-sudo ln -s ~/vimrc/bin/infinity /usr/local/bin/
-sudo ln -s ~/vimrc/bin/auth-rails /usr/local/bin/
-sudo ln -s ~/vimrc/bin/dotfiles-ruby /usr/local/bin/
+sed '/^export PATH=~\/vimrc\/bin\:\$PATH/d' ~/.bashrc > ~/vimrc/tmp/file && mv ~/vimrc/tmp/file ~/.bashrc
+echo 'export PATH=~/vimrc/bin:$PATH' >> ~/.bashrc
