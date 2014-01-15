4537d979e9a499ef2749010f68d817ff47ab32c3
Cristian Mircea Messel <mess110@gmail.com>
2014-01-15 21:26:18 +0200

    move weather script to bin folder

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-mkdir ~/vimrc/tmp
+# Prepare temp folder
+mkdir -p ~/vimrc/tmp
 
+# Mark all files within bin/ as executable
+chmod +x bin/*
+
+# Configure vim
 rm -rf ~/.vimrc
 cp .vimrc ~
 cp -R .vim ~
 
+# Configure Conky (also uses bin/weather)
 cp .conkyrc ~
-chmod +x weather.py
 
 git config --global user.name "Cristian Mircea Messel"
 # git config --global user.email mess110@gmail.com
@@ -13,7 +18,8 @@ git config --global core.editor "vim"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global core.excludesfile ~/vimrc/.gitignore_global
 
-chmod +x bin/*
-
+# Add ~/vimrc/bin to PATH
+# It makes sure the export command is only written once in ./bashrc
+# If sed fails, mv is not executed
 sed '/^export PATH=~\/vimrc\/bin\:\$PATH/d' ~/.bashrc > ~/vimrc/tmp/file && mv ~/vimrc/tmp/file ~/.bashrc
 echo 'export PATH=~/vimrc/bin:$PATH' >> ~/.bashrc
