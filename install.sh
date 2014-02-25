6752c5c418337fe0c9afc206c3e555ce7380d6bd
Cristian Mircea Messel <cristian.messel@smarttech2000.com>
2014-02-25 15:07:30 +0200

    tmux autostart

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Prepare temp folder
 mkdir -p ~/vimrc/tmp
 
@@ -17,9 +19,16 @@ git config --global user.name "Cristian Mircea Messel"
 git config --global core.editor "vim"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global core.excludesfile ~/vimrc/.gitignore_global
+git config --global color.ui true
+
+# TODO optimize this
 
 # Add ~/vimrc/bin to PATH
 # It makes sure the export command is only written once in ./bashrc
 # If sed fails, mv is not executed
 sed '/^export PATH=~\/vimrc\/bin\:\$PATH/d' ~/.bashrc > ~/vimrc/tmp/file && mv ~/vimrc/tmp/file ~/.bashrc
 echo 'export PATH=~/vimrc/bin:$PATH' >> ~/.bashrc
+
+# Add tmux to .bashrc
+sed '/^\[\[\ \$TERM\ \!\=\ \"screen\"\ \]\]\ \&\&\ exec tmux/d' ~/.bashrc > ~/vimrc/tmp/file && mv ~/vimrc/tmp/file ~/.bashrc
+echo '[[ $TERM != "screen" ]] && exec tmux' >> ~/.bashrc
