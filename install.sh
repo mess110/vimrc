079246e867445b18f027a3c21466019d5c7e9bce
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-11-14 11:13:31 +0200

    show date in git lg

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ cp .ctags ~
 git config --global user.name "Cristian Mircea Messel"
 # git config --global user.email mess110@gmail.com
 git config --global core.editor "vim"
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log  --date=iso --color --graph --pretty=format:'%Cred%h%Creset - %C(green)%ad%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global core.excludesfile ~/vimrc/.gitignore_global
 git config --global color.ui true
 
