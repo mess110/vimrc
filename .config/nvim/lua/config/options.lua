0ac939d83589dc14e80de5dea9e932ce9bd97efb
Cristian Mircea Messel <mess110@gmail.com>
2026-01-05 00:38:30 +0200

    disable conceal and map ; to :

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable conceal for markdown and tex
+-- this disables that ```sh hover
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "tex" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+    vim.opt_local.concealcursor = ""
+  end,
+})
